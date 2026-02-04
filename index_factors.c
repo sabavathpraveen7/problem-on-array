40bd3ceea276d95c12969c36aeb35eecb737f05b
sabavathpraveen7 <sabavathpraveenchinna@gmail.com>
2026-02-04 15:01:07 +0530

    Add files via upload

---
+++ index_factors.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+    int size;
+    int i;
+    printf("enter the elements to store \n");
+    scanf("%d",&size);
+    int a[size];
+    for(int i=0;i<=size-1;i++)
+    {
+printf("enter the %d element\n",(i+1));
+scanf("%d",&a[i]);
+}
+int sum=0;
+for(int i=0;i<=size-1;i++)
+{
+  sum=sum+a[i];}
+  printf("the sum is %d\n",sum);
+  for(i=1;i<=size-1;i++)
+  {
+if(sum%i==0)
+{
+printf("%d\n",i);
+}
+}
+}
